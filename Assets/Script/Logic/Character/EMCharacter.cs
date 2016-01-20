e17f1278d9fb48e704854266a0a3cec6f2dba8ec
krseong <skleen@hanmail.net>
2016-01-20 23:56:21 +0900

    캐릭터 데이터
    캐릭터 데이터 생성까지

--- Assets/Script/Logic/Character/EMCharacter.cs
+++ Assets/Script/Logic/Character/EMCharacter.cs
@@ -9,7 +9,7 @@ namespace Character
 		/// <summary>
 		/// 캐릭터 상태값, 처음시작하는경우 초기값이 설정된다.
 		/// </summary>
-		private EMDataCharacter m_Status;
+		private EMDataCharacter m_Data;
 
 		/// <summary>
 		/// Init this instance.
@@ -18,19 +18,23 @@ namespace Character
 		{
 		}
 
-		/*
+
 		public void DataSetting_Temp ()
 		{
-			m_Status = new EMDataCharacter ();
+			m_Data = new EMDataCharacter ();
 
 			// EMScheduleType.STUDY
-			m_Status.SetScheduleStatus(EMScheduleType.STUDY, MakeData_Temp(1));
+			m_Data.SetScheduleStatus(EMScheduleType.STUDY, MakeData_Temp(1));
 			
 			// EMScheduleType.JOB
-			m_Status.SetScheduleStatus(EMScheduleType.JOB, MakeData_Temp(1));
+			m_Data.SetScheduleStatus(EMScheduleType.JOB, MakeData_Temp(1));
 			
 			// EMScheduleType.VACATION
-			m_Status.SetScheduleStatus(EMScheduleType.VACATION, MakeData_Temp(1));
+			m_Data.SetScheduleStatus(EMScheduleType.VACATION, MakeData_Temp(1));
+
+			EMDataCharacterStatus userStatus = new EMDataCharacterStatus ();
+
+			m_Data.SetUserStatus (userStatus);
 		}
 
 		private EMDataScheduleStatus MakeData_Temp ( int p_Level )
@@ -40,7 +44,7 @@ namespace Character
 			
 			return data;
 		}
-		*/
+
 		
 		/// <summary>
 		/// 캐릭터의 설정값을 기준으로 가능한 스케쥴을 설정한다.
@@ -50,7 +54,7 @@ namespace Character
 		#if _Log_
 			Debug.Log ("[EMCharacter].(ScheduleSetting) call");
 		#endif
-			EMSchedulerMgr.Instance.ReqScheduleList (m_Status);
+			EMSchedulerMgr.Instance.ReqScheduleList (m_Data);
 		}
 	}
 }
--- Assets/Script/Logic/Character/EMDataCharacter.cs
+++ Assets/Script/Logic/Character/EMDataCharacter.cs
@@ -15,9 +15,47 @@ namespace Character
 		}
 	}
 
+	public class EMDataCharacterStatus
+	{
+		public int Hp;
+		public int Stress;
+		public int Concentration;
+		public int Mathmatics;
+		public int ForeignLanguage;
+		public int NativeLanguage;
+		public int CommonSense;
+
+		public int Luck;
+
+		public EMDataCharacterStatus ()
+		{
+			this.Hp = 100;
+			this.Stress = 0;
+			this.Concentration = 0;
+			this.Mathmatics = 0;
+			this.ForeignLanguage = 0;
+			this.NativeLanguage = 0;
+			this.CommonSense = 0;
+			this.Luck = 0;
+		}
+
+		public void Copy (EMDataCharacterStatus p_Data)
+		{
+			this.Hp = p_Data.Hp;
+			this.Stress = p_Data.Stress;
+			this.Concentration = p_Data.Concentration;
+			this.Mathmatics = p_Data.Mathmatics;
+			this.ForeignLanguage = p_Data.ForeignLanguage;
+			this.NativeLanguage = p_Data.NativeLanguage;
+			this.CommonSense = p_Data.CommonSense;
+			this.Luck = p_Data.Luck;
+		}
+	}
+
 	public class EMDataCharacter
 	{				
 		private Dictionary<EMScheduleType, EMDataScheduleStatus> m_ScheduleStatus = new Dictionary<EMScheduleType, EMDataScheduleStatus>();
+		private EMDataCharacterStatus m_UserStatus = new EMDataCharacterStatus();
 
 		//-GetScheduleStatus-
 		public EMDataScheduleStatus GetScheduleStatus ( EMScheduleType p_Type)
@@ -28,6 +66,16 @@ namespace Character
 		public void SetScheduleStatus (EMScheduleType p_Type, EMDataScheduleStatus p_ScheduleStatus)
 		{
 			m_ScheduleStatus.Add (p_Type, p_ScheduleStatus);
+		}
+		//-GetUserStatus-
+		public EMDataCharacterStatus GetUserStatus ()
+		{
+			return m_UserStatus;
+		}
+		//-SetUserStatus-
+		public void SetUserStatus (EMDataCharacterStatus p_UserStatus)
+		{
+			m_UserStatus.Copy (p_UserStatus);
 		}
 	}
 }
